d74531e590ac0931d1be193a693c1d2070f575f5
James Magahern <james@magahern.com>
2018-12-19 18:30:37 -0800

    Remove usage crap

--- src/main.cpp
+++ src/main.cpp
@@ -6,38 +6,14 @@
 #include "main_scene.h"
 #include "label_actor.h"
 
-void print_usage(char *progname)
-{
-    printf("%s by buzzert 2016\n", progname);
-    printf("usage: %s width height [-w]\n", progname);
-    printf("\t-w : windowed mode\n");
-}
-
 int main(int argc, char **argv)
 {
-    int canvasWidth = 64;
-    int canvasHeight = 160;
+    int canvasWidth = 640;
+    int canvasHeight = 480;
     bool windowed = false;
 
-    if (argc < 3) {
-        print_usage(basename(argv[0]));
-        return 1;
-    }
-
-    for (unsigned int i = 1; i < argc; i++) {
-        switch (i) {
-        case 1:
-            canvasWidth = atoi(argv[i]);
-            break;
-        case 2:
-            canvasHeight = atoi(argv[i]);
-            break;
-        case 3:
-            if (strcmp("-w", argv[i]) == 0) {
-                windowed = true;
-            }
-            break;
-        }
+    if (argc > 1 && strcmp("-w", argv[1]) == 0) {
+        windowed = true;
     }
 
     Rect canvasRect(0, 0, canvasWidth, canvasHeight);
