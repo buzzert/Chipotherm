a1ecba617c1a0ee209d4b2aaadabb02de13bcf37
James Magahern <james@magahern.com>
2019-01-27 21:31:00 -0800

    Little deploy script to make copying to chip easier

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: deploy.sh [chip ip address]"
+    echo "Make sure to build with arm toolchain first"
+    exit 1
+fi
+
+scp chipotherm "root@$1:~/chipotherm" && ssh root@$1 "DISPLAY=:0 ~/chipotherm"
+
