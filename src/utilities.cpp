b3768ee0b384728ef08bbfe50920c2173ff30b62
James Magahern <james@magahern.com>
2019-12-18 20:39:57 -0800

    Should use REALTIME_COURSE for animations instead of monotonic

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -10,7 +10,7 @@ namespace Utilities {
     double time_now()
     {
         struct timespec ts;
-        clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
+        clock_gettime(CLOCK_REALTIME_COARSE, &ts);
 
         long ms = (ts.tv_sec * 1000) + (ts.tv_nsec / 1000000);                                                           
         return (ms / 1000.0);
