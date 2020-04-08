ed352226355257550bdb836174583af4eb2b1d1b
James Magahern <james@magahern.com>
2020-04-07 22:27:44 -0700

    build_device: Name build products with version

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN apt-get update;\
         build-essential \
         pkg-config \
         cmake \
+        git \
         libx11-dev libboost-dev libhidapi-dev libcairo2-dev \
         libsoup2.4-dev libpango1.0-dev libxfixes-dev libcanberra-dev libudev-dev
 
@@ -16,4 +17,6 @@ WORKDIR /build
 RUN cmake /src
 RUN make clean
 RUN make -j4
+
+RUN mv /build/src/chipotherm /build/src/chipotherm-$(git -C /src describe)
 
--- build_device.sh
+++ build_device.sh
@@ -4,9 +4,11 @@ echo Building chipotherm
 
 docker build -t buzzert/chipotherm:build .
 
-mkdir build_device
+VERS=$(git describe)
+
+mkdir -p build_device
 docker container create --name extract buzzert/chipotherm:build
-docker container cp extract:/build/src/chipotherm ./build_device
+docker container cp extract:/build/src/chipotherm-$VERS ./build_device
 docker container rm -f extract
 
 echo Built product \'chipotherm\' in build_device
